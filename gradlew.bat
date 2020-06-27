e224005c18972f9b566059f0faa1d0f7ac5e9872
henok <henokgetachew500@gmail.com>
2020-06-27 14:41:06 +0300

    first

---
+++ gradlew.bat
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<project version="4">
+  <component name="AndroidConfiguredLogFilters">
+    <filters>
+      <filter>
+        <option name="logLevel" value="debug" />
+        <option name="logMessagePattern" value="" />
+        <option name="logTagPattern" value="main" />
+        <option name="name" value="Main" />
+        <option name="packageNamePattern" value="" />
+        <option name="pid" value="" />
+      </filter>
+    </filters>
+  </component>
+  <component name="AndroidGradleBuildConfiguration">
+    <option name="COMMAND_LINE_OPTIONS" value="--stacktrace" />
+  </component>
+  <component name="AndroidLayouts">
+    <shared>
+      <config />
+    </shared>
+  </component>
+  <component name="AndroidLogFilters">
+    <option name="TOOL_WINDOW_CUSTOM_FILTER" value="fata" />
+    <option name="TOOL_WINDOW_LOG_LEVEL" value="error" />
+    <option name="TOOL_WINDOW_CONFIGURED_FILTER" value="No Filters" />
+  </component>
+  <component name="ChangeListManager">
+    <list default="true" id="f183f4bd-befd-4d55-962c-38d5a36b50f1" name="Default Changelist" comment="">
+      <change beforePath="$PROJECT_DIR$/app/build.gradle" beforeDir="false" afterPath="$PROJECT_DIR$/app/build.gradle" afterDir="false" />
+      <change beforePath="$PROJECT_DIR$/app/src/main/java/com/hfad/criminalintentkotlini/CrimeProvider.kt" beforeDir="false" afterPath="$PROJECT_DIR$/app/src/main/java/com/hfad/criminalintentkotlini/CrimeProvider.kt" afterDir="false" />
+      <change beforePath="$PROJECT_DIR$/app/src/main/java/com/hfad/criminalintentkotlini/Model/DataManager.kt" beforeDir="false" afterPath="$PROJECT_DIR$/app/src/main/java/com/hfad/criminalintentkotlini/Model/DataManager.kt" afterDir="false" />
+      <change beforePath="$PROJECT_DIR$/app/src/main/java/com/hfad/criminalintentkotlini/Model/Database/Room/Crime.kt" beforeDir="false" afterPath="$PROJECT_DIR$/app/src/main/java/com/hfad/criminalintentkotlini/Model/Crime.kt" afterDir="false" />
+      <change beforePath="$PROJECT_DIR$/app/src/main/java/com/hfad/criminalintentkotlini/Model/Database/Room/CrimeDB.kt" beforeDir="false" afterPath="$PROJECT_DIR$/app/src/main/java/com/hfad/criminalintentkotlini/Model/CrimeDB.kt" afterDir="false" />
+      <change beforePath="$PROJECT_DIR$/app/src/m
