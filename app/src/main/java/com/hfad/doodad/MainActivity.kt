5f68e2e8395cf64d16ddbc0bf2b182f709deb878
henok <henokgetachew500@gmail.com>
2020-07-01 00:30:24 +0300

    ui working with errror

--- app/src/main/java/com/hfad/doodad/MainActivity.kt
+++ app/src/main/java/com/hfad/doodad/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.hfad.doodad
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.*
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+  private lateinit var navController : NavController
+  private lateinit var drawerLayout : DrawerLayout
+  private lateinit var navigationView : NavigationView
+  private lateinit var appBarConfiguration : AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+
+        navController = findNavController(R.id.navHostFragment)
+         drawerLayout = findViewById ( R.id.drawer_layout)
+         navigationView = findViewById (R.id.navigation_view)
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navHome,
+                R.id.navStatistics
+            ), drawerLayout
+        )
+
+        NavigationUI.setupWithNavController(navigationView, navController)
+        setupActionBarWithNavController(navController,appBarConfiguration)
+        navigationView.setupWithNavController(navController)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return item.onNavDestinationSelected( navController ) || super.onOptionsItemSelected(item)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/com/hfad/doodad/dataLayer/database/TaskDao.kt
+++ app/src/main/java/com/hfad/doodad/dataLayer/database/TaskDao.kt
@@ -1,9 +1,10 @@
 package com.hfad.doodad.dataLayer.database
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 
 @Dao
 interface TaskDao
 {
-
+    fun observeAll() : LiveData< List<Task>>
 }
